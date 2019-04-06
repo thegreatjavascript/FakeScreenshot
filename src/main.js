981b75bcb1cce449e90f47e47f1206fb461748d0
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2019-04-06 09:02:44 +0800

    添加进度条动画

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueProgressBar from 'vue-progressbar'
 import App from './App'
 import router from './router'
 import { Button, Dialog, Icon, Card, Upload, Slider } from 'element-ui'
@@ -12,11 +13,12 @@ Vue.use(Icon)
 Vue.use(Card)
 Vue.use(Upload)
 Vue.use(Slider)
+Vue.use(VueProgressBar)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+window.app = new Vue({
   el: '#app',
   router,
   render: h => h(App)
